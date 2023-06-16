ac20bed04ef7260a22121ee34385f1fbf92f7eb7
RayNjoroge <raymond30049@gmail.com>
2023-06-15 20:51:33 -0700

    C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+/*include header*/
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - this is the entry point
+ *const char - to declare constant string.
+ *size_t msg_len - to find the size of the string.
+ *write function - to write the required string literal to standard error.
+ *Return: -  returns 1.
+ */
+int main(void)
+{
+	const char *msg = "and that piece of art is useful\"-Dora Korpar,2015-10-19";
+	size_t msg_len = strlen(msg);
+
+	write(STDERR_FILENO, msg, msg_len);
+
+	return (1);
+}
