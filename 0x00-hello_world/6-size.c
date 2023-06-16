28d64f5ce0d7934d4be325072a6290fe710ba6f6
RayNjoroge <raymond30049@gmail.com>
2023-06-15 19:15:55 -0700

    C program that prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,21 +2,17 @@
 #include <stdio.h>
 /**
  * main - this is the entry point.
- * myArray - containing the various types.
- * sizeTypes - containing the array values.
  * printf - prints the value of the sizes of the types.
+ * sizeof function - to evaluate sizes of varios types
  * Return: -returns 0
  */
 int main(void)
 {
-	int myArray[5] = {char, int, long int, long long int, float};
-	int sizeTypes = sizeof(myArray[0]);
-
-	printf("Size of a char: %d byte(s)\n", myArray[0]);
-	printf("Size of an int: %d byte(s)\n", myArray[1]);
-	printf("Size of a long int: %d byte(s)\n", myArray[2]);
-	printf("Size of a long long int: %d byte(s)\n", myArray[3]);
-	printf("Size of a float: %d byte(s)\n", myArray[4]);
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
 	return (0);
 }
