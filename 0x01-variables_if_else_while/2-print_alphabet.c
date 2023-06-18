f6aec21773c8bf19de55d95f7db7bbbe6a849069
RayNjoroge <raymond30049@gmail.com>
2023-06-18 10:09:37 -0700

    A program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*include the necessary libraries*/
+#include <stdio.h>
+
+/**
+ *main function - this is the entry point
+ *declare a variable to hold the individual letters of the alphabet
+ *for loop - to define the conditions to be checked
+ *putchar() - to print the value of the variable(alphabet)
+ *Return: - to exit the program; returns 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+		putchar('\n');
+	}
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 Task - 0 Program that will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.'\n'
 Task - 1 Program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.'\n'
+Task - 2 A program that prints the alphabet in lowercase, followed by a new line.'\n'
