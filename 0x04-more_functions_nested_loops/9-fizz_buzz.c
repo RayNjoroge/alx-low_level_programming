bbd0ae505a8733a0cf998667515760008cf993cd
RayNjoroge <raymond30049@gmail.com>
2023-06-23 19:38:05 -0700

    Fizz-Buzz. A program that prints the numbers from 1 to 100, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+/*include header file(s) containing necessary functions*/
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * i - variable of type int
+ * for loop to set conditions to ++ to 100
+ * if..else if statements - for setting conditions
+ * printf: - to print the output
+ * Return: - return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 15 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
