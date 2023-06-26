2776a71804c74853d1f21dbb49e56c7193b4b9f2
RayNjoroge <raymond30049@gmail.com>
2023-06-26 15:58:20 -0700

    A function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/*include header file(s) containing necessary functions*/
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ * @n: - value will be 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
