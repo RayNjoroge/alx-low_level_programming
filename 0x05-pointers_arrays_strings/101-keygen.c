1d0efff163f74961dff004351dc346fbd8f6ba9c
RayNjoroge <raymond30049@gmail.com>
2023-06-27 17:59:54 -0700

    A program that generates random valid passwords for the program 101-crackme.

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,43 @@
+/*include header file(s) containing necessary functions*/
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * pwd - variable
+ * for loop & if...statements
+ * Return: - return 0
+ */
+int main(void)
+{
+	int PASSWORD_LENGTH = 10;
+
+	srand(time(NULL));
+
+	char pwd[PASSWORD_LENGTH + 1];
+
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		int random_num = rand() % 62;
+
+		if (random_num < 26)
+		{
+			pwd[i] = 'A' + random_num;
+		}
+		else if (random_num < 52)
+		{
+			pwd[i] = 'a' + (random_num - 26);
+		}
+		else
+		{
+			pwd[i] = '0' + (random_num - 52);
+		}
+	}
+	pwd[PASSWORD_LENGTH] = '\0';
+
+	printf("Random Password: %s\n", pwd);
+
+	return (0);
+}
