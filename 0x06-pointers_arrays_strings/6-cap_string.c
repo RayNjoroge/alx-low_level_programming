b9a64e8b001f57af4c4ce99de1db841bc3db9965
RayNjoroge <raymond30049@gmail.com>
2023-06-30 19:17:08 -0700

    A function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+/*include header file(s) containing necessary functions*/
+#include "main.h"
+
+/**
+ * cap_string - entry point
+ * @ptr: - param 1
+ * count - variable
+ * if statements
+ * Return: - returns ptr
+ */
+
+char *cap_string(char *ptr)
+{
+	int count;
+
+	count = 0;
+	while (ptr[count] != '\0')
+	{
+		if (ptr[0] >= 97 && ptr[0] <= 122)
+		{
+			ptr[0] = ptr[0] - 32;
+		}
+
+		if (ptr[count] == ' ' || ptr[count] == '\t' || ptr[count] == '\n'
+			|| ptr[count] == ',' || ptr[count] == ';' || ptr[count] == '.'
+			|| ptr[count] || ptr[count] == '!' ||  ptr[count] == '?'
+			|| ptr[count] == '"' || ptr[count] == '('
+			|| ptr[count] == ')' || ptr[count] == '{' || ptr[count] == '}')
+		{
+			if (ptr[count + 1] >= 97 && ptr[count + 1] <= 122)
+			{
+				ptr[count + 1] = ptr[count + 1] - 32;
+			}
+		}
+		count++;
+	}
+	return (ptr);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@ Task - 2 A function that copies a string. <br>
 Task - 3 A function that compares two strings. <br>
 Task - 4 A function that reverses the content of an array of integers. <br>
 Task - 5 A function that changes all lowercase letters of a string to uppercase. <br>
+Task - 6 A function that capitalizes all words of a string. <br>
