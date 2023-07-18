4c71483ec2717f2559d4dcc5249ddf8785d244da
RayNjoroge <raymond30049@gmail.com>
2023-07-18 16:13:54 -0700

    A new type struct dog with the given elements.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure with attributes
+ * @name: - param 1
+ * @age: - param 2
+ * @owner: param 3
+ * Description: - longer description
+ */
 struct dog
 {
 	char *name;
