f40361d17e1037551af2f38977c3ab57205eb3bb
RayNjoroge <raymond30049@gmail.com>
2023-07-19 05:58:11 -0700

    Task - 1 A function that initialize a variable of type struct dog.

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+/*include header file(s) containing necessary files*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - entry point
+ * @d: - param 1
+ * @name: - param 2
+ * @age: - param 3
+ * @owner: - param 4
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+/*include header file(s) containing necessary functions*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - entry point
+ * @d: - param 1
+ * Description: more description
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+	  return;
+	}
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\n", d->name);
+	printf("Age: %.f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
