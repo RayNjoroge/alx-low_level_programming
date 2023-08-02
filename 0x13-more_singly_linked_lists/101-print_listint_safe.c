a01fdb9937fb6276e122d5bff3814aae2b8a45a0
RayNjoroge <raymond30049@gmail.com>
2023-08-01 19:37:26 -0700

    0x13. C - More singly linked lists - Tasks 10 -11

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,22 @@
+/*include header file(s) containing necessary dependencies*/
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ */
+int find_loop(const listint_t *head)
+{
+	const listint_t *
+}
+
+/**
+ * print_listint_safe - function that prints a listint_t linked list
+ * @head: - pointer to head node
+ * Return: returns no of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+
+}
