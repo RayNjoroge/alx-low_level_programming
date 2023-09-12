042c1af358f70750e042e95a37df62b20788bb87
RayNjoroge <raymond30049@gmail.com>
2023-09-12 12:42:15 -0700

    Task - 2 A function that prints a struct dog.

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,6 +17,15 @@ void print_dog(struct dog *d)
 	}
 
 	printf("Name: %s\n", (d->name != NULL) ? d->name : ("nil"));
-	printf("Age: %f\n", d->age);
+
+	if (d->age < 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+
 	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : ("nil"));
 }
