867ff96ff2195e4eb5a474d44bed5311c61ca492
RayNjoroge <raymond30049@gmail.com>
2023-09-14 05:24:30 -0700

    Task - 2 A function that searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,43 @@
+/*Include necessary dependencies*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - main fn
+ * @array: - param 1
+ * @size: - size of the array
+ * @cmp: - param 2
+ *
+ * Return: Nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
+
+/**
+ * even_No - main fn
+ * @value: - no to check whether it is even
+ *
+ * Return: - return 0
+ */
+int even_No(int value)
+{
+	return (value % 2 == 0);
+}
