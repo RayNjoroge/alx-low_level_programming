0c425e2f4db399b7355dda476e1fd0a5524bb225
RayNjoroge <raymond30049@gmail.com>
2023-09-27 06:23:15 -0700

    Task - 9 A function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,24 +1,29 @@
 /*include header file(s) containing necessary dependencies*/
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * insert_nodeint_at_index - function that inserts a new
- *                              node at a given position
+ *				node at a given position
  * @head: - double pointer to head node
  * @idx: - index of the list where the
  *              new node should be added
  * @n: - value being stored in the new node
  * Return: - returns address of hte new node
- *              and NULL if it failed
- *              return NULL if it's not possible to add the new
- *              node
+ *		and NULL if it failed
+ *		return NULL if it's not possible to add the new
+ *		node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *nw_node = (listint_t *)malloc(sizeof(listint_t));
-
-	listint_t *current_node = *head;
+	listint_t *nw_node, *current_node;
 	unsigned int current_idx = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	nw_node = malloc(sizeof(listint_t));
 
 	if (nw_node == NULL)
 	{
@@ -26,30 +31,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	nw_node->n = n;
+	current_node = *head;
 
 	if (idx == 0)
 	{
-		nw_node->next = *head;
+		nw_node->next = current_node;
 		*head = nw_node;
+
+		return (nw_node);
 	}
-	else
+
+	while (current_node != NULL)
 	{
-		while (current_node != NULL && current_idx < idx - 1)
+		if (current_idx == idx - 1)
 		{
-			current_node = current_node->next;
-			current_idx++;
+			nw_node->next = current_node->next;
+			current_node->next = nw_node;
+			return (nw_node);
 		}
 
-		if (current_node == NULL || current_idx != idx - 1)
-		{
-			free(nw_node);
-
-			return (NULL);
-		}
-
-		nw_node->next = current_node->next;
-		current_node = nw_node;
+		current_node = current_node->next;
+		current_idx++;
 	}
 
-	return (nw_node);
+	free(nw_node);
+
+	return (NULL);
 }
