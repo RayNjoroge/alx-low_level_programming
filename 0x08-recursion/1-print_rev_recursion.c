0225f2c8c36526124018de8a0890a797148f26a1
RayNjoroge <raymond30049@gmail.com>
2023-10-25 06:35:11 -0700

    ./1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,11 +11,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (s[0] == '\n')
+	if (*s != '\0')
 	{
-		return;
-	}
+		_print_rev_recursion(s + 1);
 
-	_print_rev_recursions(s + 1);
-	_putchar(s[0]);
+		_putchar(*s);
+	}
 }
