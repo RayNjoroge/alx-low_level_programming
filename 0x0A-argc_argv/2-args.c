689e5e31f385a2e95c1ea915587f2580c720ba0e
RayNjoroge <raymond30049@gmail.com>
2023-10-27 11:48:37 -0700

    Task - 2 A program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+/*Include necessary file dependencies*/
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main fn
+ * @argc: - param 1
+ * @argv: - param 2
+ *
+ * Return: - return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
